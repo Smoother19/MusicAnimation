46dbf7116540ba0eb621cefb591ebb36417286ce
Gabriel <gabriel@ik.me>
2026-08-20 18:16:49 +0200

    Reverted to working mp3tomidi algo

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import midiDecoder
 inputfile = "PinkPanther_Both.mp3"
 
 def main() :
-    #isMidi = mp3toMidi.decode(inputfile)
-    isMidi = mp3toAprèsMidi.decode(inputfile)
+    isMidi = mp3toMidi.decode(inputfile)
+    #isMidi = mp3toAprèsMidi.decode(inputfile)
     midi_array, bpm = midiDecoder.decode() # RETURNS AS ARRAY : [STARTTIME;ENDTIME;NOTE(Hz)] & the BPM
     gui.start_gui(isMidi)
 
