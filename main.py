b32c925672dbe0819341a2b80ed2406c4015989c
Gabriel <gabriel@ik.me>
2026-08-27 23:46:21 +0200

    La Musica pour la pres

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import midiDecoder
 import mp3toMidi_RMS
 import mp3toMidi_salience
 
-inputfile = "PinkPanther_Piano_Only.mp3"
+inputfile = "La_Pantera.mp3"
 
 def main():
     #isMidi = mp3toMidi.decode(inputfile)
-    #isMidi = mp3toMidi_RMS.decode(inputfile)
-    isMidi = mp3toMidi_salience.decode(inputfile)
+    isMidi = mp3toMidi_RMS.decode(inputfile)
+    #isMidi = mp3toMidi_salience.decode(inputfile)
 
     midi_array, bpm = midiDecoder.decode()
     gui.start_gui(isMidi)
